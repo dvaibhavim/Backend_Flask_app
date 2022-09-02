2ef0a6df5a7296540ec98cbd927dd773fe899c28
dvaibhavim <deshpamdevai@gmail.com>
2022-09-02 17:21:20 +0530

    added api to return avergae difficulty of songs

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 from pymongo import MongoClient
 from flask import jsonify
 import json
 from bson import json_util
 from pymongo.cursor import Cursor
 from flask_pymongo import PyMongo
+from functools import reduce
 
 
 app = Flask(__name__)
@@ -16,7 +17,7 @@ songs_music = db.songs
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+	return "Hello, World!"
 	
 @app.route("/songs/", methods=['GET'])
 def songs():
@@ -28,10 +29,29 @@ def songs():
 		cnt += 1
 	return jsonify(result)
 	
+@app.route("/songs/avg/difficulty/", methods=['GET'])	
+def get_avg_difficulty():
+	all_songs = db.songs.find()
+	columns = {'_id': 0, 'difficulty': 1}
+
+	level = request.args.get('level')
+	where = {}
+
+	if level:
+		where['level'] = int(level)
+
+	_songs = list(map(lambda song: song['difficulty'], db.songs.find(where, columns)))
+	total_items = len(_songs)
+	total_value = reduce(lambda total, value: total + (value), _songs, 0)
+	result =  round(total_value / total_items, 2)
+
+	print("avg diff songs", _songs, result)
+	return jsonify({"average difficulty": result})
 	
+
+		
 	
-    
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
 
 
